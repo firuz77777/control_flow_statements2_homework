86368f3c8e5e89df43e437ee911f075476b6ac6b
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-30 21:23:35 +0500

    02

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    n = a 
+    if a > b and b > c or c > b and b > a:
+        n = b
+    if b > a and a > c or c > a and a > b:
+        n = a 
+    if a > c and c > b or b > c and c > a:
+        n = c
+    return n
