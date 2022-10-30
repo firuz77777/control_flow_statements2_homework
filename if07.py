2e60d347fe9afa0e2c67f1fd0e189de4e7476e3a
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-30 22:21:11 +0500

    d

--- if07.py
+++ if07.py
@@ -13,4 +13,17 @@ def main(temp):
     Returns:
         str: return answer.
     """
-    return
+    a = 0
+    if temp == 0:
+        a = "Freezing"
+    elif temp == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10:
+        a = "Very Cold"
+    elif temp == 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20:
+        a = "Cold"
+    elif temp == 21 or 22 or 23 or 24 or 25 or 26 or 27 or 28 or 29 or 30:
+        a = "Normal"
+    elif temp == 31 or 32 or 33 or 34 or 35 or 36 or 37 or 38 or 39 or 40:
+        a = "Hot"
+    elif temp > 40:
+        a = "Very Hot"
+    return a
